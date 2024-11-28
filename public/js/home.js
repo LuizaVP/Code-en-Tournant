5c4c7b54ee2e799cc9b0145f9933cb9cb1e56c32
LuizaVP <luiza.pompermayer@sptech.school>
2024-11-28 11:08:54 -0300

    Adicionando a validação de sessão

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,8 @@
-/* function validarSessao(){
+function validarSessao(){
     var idUsuario = sessionStorage.ID_USUARIO;
     console.log(idUsuario);
     if(idUsuario != null || idUsuario != undefined){
         menu.innerHTML += `<a href='/perfil.html'>PERFIL</a>`;
         boxLogin.innerHTML = "";
-    }else{
-        location.replace("/login1.html")
     }
-
-} */
+}
